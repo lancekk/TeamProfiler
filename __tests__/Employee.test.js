452d6851cb86c248829a4cec398adde4169ca927
Lance Kelley <lancekkcodes@gmail.com>
2021-10-17 20:58:24 -0500

    employee properties tests and code

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,7 +1,12 @@
-const { test } = require('@jest/globals');
+const { test, expect } = require('@jest/globals');
 let Employee = require('../lib/Employee');
 
 test('Creates an Employee', () => {
   const Emp = new Employee("John", 1234, "John@example.com");
+
+  // check properties
+  expect(Emp.name).toEqual("John");
+  expect(Emp.id).toEqual(1234);
+  expect(Emp.email).toEqual("John@example.com");
 })
 
